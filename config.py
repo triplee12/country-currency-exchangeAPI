613357152df98ba0a062304bfd742832a1e727d1
triplee12 <tripleeoliver@gmail.com>
2025-10-26 18:18:59 +0100

    Fixed pydantic setting

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -10,8 +10,10 @@ class Settings(BaseSettings):
     EXCHANGE_URL: str = 'https://open.er-api.com/v6/latest/USD'
     CACHE_DIR: str = 'cache'
 
-    class Config:
-        env_file = '.env'
+    model_config = SettingsConfigDict(
+    	env_file=".env",
+    	env_file_encoding="utf-8"
+    )
 
 @lru_cache()
 def get_settings():
