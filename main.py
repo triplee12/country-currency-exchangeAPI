c8bb4303e453dd1c3a50f5e2c02d080ae0967faf
triplee12 <tripleeoliver@gmail.com>
2025-10-26 16:57:45 +0100

    Implemented entry point

---
+++ main.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from db.database import database, engine, metadata
+from routes.countries import router as countries_router
+import uvicorn
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    metadata.create_all(bind=engine)
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(
+    title="Country Currency Exchange API",
+    lifespan=lifespan
+)
+app.include_router(countries_router, prefix="/countries")
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
